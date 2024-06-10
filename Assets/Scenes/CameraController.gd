42100c09fce47221a451e4118938127c431cdc46
George-Gardiner <georgecamg@gmail.com>
2024-06-10 17:46:24 -0400

    added camera control with mouse. camera oreantation doesn't affect movement yet

--- Assets/Scenes/CameraController.gd
+++ Assets/Scenes/CameraController.gd
@@ -1,11 +1,18 @@
 extends Node3D
 
+const turnSpeed = -0.2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(event.get_relative().x*turnSpeed))
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass # rotate_y(deg2rad())
+	pass
