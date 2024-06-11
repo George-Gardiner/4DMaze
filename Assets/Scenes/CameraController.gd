fe5784eb65ba6f408e9ee04f39382e787cc47995
George-Gardiner <georgecamg@gmail.com>
2024-06-10 20:02:54 -0400

    started adding pause menu

--- Assets/Scenes/CameraController.gd
+++ Assets/Scenes/CameraController.gd
@@ -1,16 +1,19 @@
 extends Node3D
 
-const turnSpeed = -0.2
+var turnSpeed = -0.2
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Lock mouse in center of screen and hide
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(deg_to_rad(event.get_relative().x*turnSpeed))
+	elif event.is_action_pressed("Menu"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
