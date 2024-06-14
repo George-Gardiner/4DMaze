5ad5867e66ba8b21b0bb6aef797c7df246b337ff
George-Gardiner <georgecamg@gmail.com>
2024-06-14 10:05:23 -0400

    made second portal work and added save button with no function

---
+++ Assets/Scenes/Levels/Scripts/level_2.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@onready var portals = [get_node("Portal1")]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	portals[0].exit = "res://Assets/Scenes/Levels/level_1.tscn"
+	portals[0].active = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
