b22416641690ea9a662081f86b603f47f29ffafa
George-Gardiner <georgecamg@gmail.com>
2024-07-26 11:30:39 -0700

    fixed error the happens when loading level without location and implemented portal exit location in all levels. also made level 3 and 4 accessable

--- Assets/Scenes/Levels/Scripts/level_3.gd
+++ Assets/Scenes/Levels/Scripts/level_3.gd
@@ -1,12 +1,21 @@
 extends Node3D
 
 @onready var portals = [get_node("Portal1"), get_node("Portal2")]
+@onready var location
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# portal setup
 	portals[0].exit = "res://Assets/Scenes/Levels/level_4.tscn"
 	portals[0].active = true
 	portals[0].exitLocation = 1
+	
+	# teleport player
+	var file = FileAccess.open("res://Data/Location.data", FileAccess.READ)
+	location = file.get_var()
+	file.close()
+	if location != null:
+		get_node("MainPlayer").position = get_node("Location"+str(location)).position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
